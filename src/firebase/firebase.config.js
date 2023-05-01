736a13eb23defb4c11c1c5432b311c011d32af7b
Deadless-Programmer <webtuhin@gmail.com>
2023-05-02 02:20:38 +0600

    install react router dom successfully

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8XIbCvyLPg8bE24IG1XkFEWkxsQ6cNQk",
+  authDomain: "assingment10b7-project.firebaseapp.com",
+  projectId: "assingment10b7-project",
+  storageBucket: "assingment10b7-project.appspot.com",
+  messagingSenderId: "241776154689",
+  appId: "1:241776154689:web:0059dff71aa352d0940ec0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
