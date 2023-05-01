d46363db7134d50c58a324f01274222fbb48f7d1
Deadless-Programmer <webtuhin@gmail.com>
2023-05-02 03:32:52 +0600

    register and login part setup

--- src/routes/Routers/Routers.jsx
+++ src/routes/Routers/Routers.jsx
@@ -1,5 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main/Main";
+import SignIn from "../../components/SignIn/SignIn";
+import SignUp from "../../components/SignUp/SignUp";
+import Home from "../../components/Home/Home";
+import Blog from "../../components/Blog/Blog";
 
 const router = createBrowserRouter([
     {
@@ -7,7 +11,20 @@ const router = createBrowserRouter([
         element:<Main></Main>,
         children:[
             {
-                path:"/"
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/blog',
+                element:<Blog></Blog>
+            },
+            {
+                path:"/login",
+                element:<SignIn></SignIn>
+            },
+            {
+                path:'/signup',
+                element:<SignUp></SignUp>
             }
         ]
     }
--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -28,9 +29,11 @@ const Header = () => {
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Home</a></li>
+      <li className='font-semibold'><Link to="/" >Home</Link></li>
       
-      <li><a>Blog</a></li>
+      <li className='font-semibold'><Link to="/blog">Blog</Link></li>
+      <li className='font-semibold'><Link to="login">Log-in</Link></li>
+      <li className='font-semibold'><Link to="/signup">Sign-up</Link></li>
     </ul>
   </div>
   <div className="navbar-end">
