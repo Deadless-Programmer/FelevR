b5b172a1c0315a163338255045799ea380b23b96
Deadless-Programmer <webtuhin@gmail.com>
2023-05-02 09:32:28 +0600

    Chef section complete

---
+++ src/components/Data/Data.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Data = ({ data }) => {
+  const {id, chefPicture, chefName, yearsOfExperience, numbersOfRecipes, Likes } =
+    data;
+
+  return (
+    <div>
+      <div className="w-full p-6 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50">
+        <img
+          src={chefPicture}
+          alt=""
+          className="object-cover object-center w-full rounded-md h-72 dark:bg-gray-500"
+        />
+        <div className="mt-6 mb-2">
+          <span className="block text-2xl  tracking-widest font-bold dark:text-violet-400">
+            {chefName}
+          </span>
+          <div className="flex justify-between mt-5">
+            <p className="font-semibold">
+              Experience :<span className=""> {yearsOfExperience} Years </span>
+            </p>
+
+            <p className="font-semibold">Recipes : {numbersOfRecipes}</p>
+
+            <p className="font-semibold"> Likes : {Likes}</p>
+          </div>
+
+          <div className="my-5">
+            <button className="btn btn-outline btn-success">
+              View Recipes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Data;
