994e72dfd41ecadf5f31d92b937037a56c975730
Deadless-Programmer <webtuhin@gmail.com>
2023-05-02 10:40:06 +0600

    veiwDetails banner part complete

--- src/components/ViewDetails/ViewDetails.jsx
+++ src/components/ViewDetails/ViewDetails.jsx
@@ -1,14 +1,90 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const ViewDetails = () => {
-    const data = useLoaderData();
-    console.log(data);
-    return (
-        <div>
-            <h1>this is view details</h1>
-        </div>
-    );
+  const data = useLoaderData();
+  console.log(data);
+  const {
+    Bio,
+    chefPicture,
+    chefName,
+    numbersOfRecipes,
+    yearsOfExperience,
+    Likes,
+  } = data;
+  return (
+    <div className="flex justify-between p-10">
+      <div className="w-[48%]">
+        <article className="flex flex-col dark:bg-gray-900">
+          <a
+            rel="noopener noreferrer"
+            href="#"
+            aria-label="Te nulla oportere reprimique his dolorum"
+          >
+            <img
+              alt=""
+              className="object-cover w-full h-96 dark:bg-gray-500"
+              src={chefPicture}
+            />
+          </a>
+          <div className="flex flex-col flex-1 p-6">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              aria-label="Te nulla oportere reprimique his dolorum"
+            ></a>
+            <p className="text-2xl font-semibold tracking-wider uppercase hover:underline dark:text-violet-400">
+              {chefName}
+            </p>
+            <h4 className="flex-1 py-2 text-base font-semibold leading-snug">
+              {Bio}
+            </h4>
+            <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
+              <span>{numbersOfRecipes} Recipes</span>
+              <span>{yearsOfExperience} years of experience</span>
+              <span>{Likes} Likes</span>
+            </div>
+          </div>
+        </article>
+      </div>
+      <div className="w-[48%]">
+        <article className="flex flex-col dark:bg-gray-900">
+          <a
+            rel="noopener noreferrer"
+            href="#"
+            aria-label="Te nulla oportere reprimique his dolorum"
+          >
+            <img
+              alt=""
+              className="object-cover w-full h-52 dark:bg-gray-500"
+              src="https://source.unsplash.com/200x200/?fashion?1"
+            />
+          </a>
+          <div className="flex flex-col flex-1 p-6">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              aria-label="Te nulla oportere reprimique his dolorum"
+            ></a>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="text-xs tracking-wider uppercase hover:underline dark:text-violet-400"
+            >
+              Convenire
+            </a>
+            <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
+              Te nulla oportere reprimique his dolorum
+            </h3>
+            <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
+              <span>June 1, 2020</span>
+              <span>2.1K views</span>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+  );
 };
 
 export default ViewDetails;
